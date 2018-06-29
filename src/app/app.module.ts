08bd8d30f699dafd3f22ce0210b3c500b644fc06
Devraj Thakur <devrajt@xornor.co>
2018-06-29 18:36:00 +0530

    add-on firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { MyApp } from './app.component';
 import { Toast } from '@ionic-native/toast';
 import { SocialSharing } from '@ionic-native/social-sharing';
 
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+// Paste in your credentials that you saved earlier
+var firebaseConfig = {
+  apiKey: "AIzaSyC9V8QuTCFxsTuI5c8mv_zT_wy6um20kfY",
+  authDomain: "vidhi-b1fed.firebaseapp.com",
+  databaseURL: "https://vidhi-b1fed.firebaseio.com",
+  projectId: "vidhi-b1fed",
+  storageBucket: "vidhi-b1fed.appspot.com",
+  messagingSenderId: "675177043941"
+};
 
 @NgModule({
   declarations: [
@@ -16,7 +28,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFirestoreModule 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
